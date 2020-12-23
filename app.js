54a707b47ca3854e59d79dc8b5af5e628a71355c
Devesh-19 <devdeveshsingh@gmail.com>
2020-12-23 23:31:26 +0530

    Use bcrypt hashing method

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,23 +38,27 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-	const newUser = new User({
-		email: req.body.username,
-		password: md5(req.body.password),
-	});
+	bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+		// Store hash in your password DB.
 
-	newUser.save((err) => {
-		if (err) {
-			res.send(err);
-		} else {
-			res.render("secrets");
-		}
+		const newUser = new User({
+			email: req.body.username,
+			password: hash,
+		});
+
+		newUser.save((err) => {
+			if (err) {
+				res.send(err);
+			} else {
+				res.render("secrets");
+			}
+		});
 	});
 });
 
 app.post("/login", (req, res) => {
 	const email = req.body.username;
-	const password = md5(req.body.password);
+	const password = req.body.password;
 	User.findOne({ email: email }, (err, foundUser) => {
 		if (err) {
 			res.send(err);
@@ -60,11 +66,17 @@ app.post("/login", (req, res) => {
 			if (foundUser === null || foundUser === "") {
 				res.send("User not found!");
 			} else {
-				if (foundUser.password == password) {
-					res.render("secrets");
-				} else {
-					res.send("Wrong password! Try again.");
-				}
+				bcrypt.compare(
+					password,
+					foundUser.password,
+					function (err, result) {
+						if (result === true) {
+							res.render("secrets");
+						} else {
+							res.send("Wrong password! Try again.");
+						}
+					}
+				);
 			}
 		}
 	});
